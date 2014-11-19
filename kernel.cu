b409c1834d56fa24727d7a4d056578b0f70062ec
Matthew Tomei <tomei2@illinois.edu>
2014-11-19 15:29:29 -0600

    getting random values used for perturbation in kernel

--- kernel.cu
+++ kernel.cu
@@ -1,6 +1,6 @@
 #include"support.h"
 #include<cuda.h>
-#include<curand.h>
+#include<curand_kernel.h>
 #include<time.h>
 
 #define norm_vect_len(m) (sqrt(pow((m).x, 2) + pow((m).y, 2))/(m).num_elem)
@@ -56,54 +56,57 @@ __global__ void calc_energy(float *latt, unsigned int latt_len, float *nrg) {
 	}
 
 }
-
-__global__ void iterate_nrg(float temp, float *latt, unsigned int latt_len, unsigned int *rand_inds, float *rand_elems, float *rands, float *nrg) {
+__global__ void iterate_nrg(float temp, float *latt, unsigned int latt_len, int num_steps, curandState *states, float *nrg) {
   int index = blockIdx.x * BLOCK_SIZE + threadIdx.x;
-	if(index >= latt_len) return;
-  unsigned int rand_ind = rand_inds[index] % latt_len;
-  float new_val = rand_elems[index] * PI_2;
-  float delta_nrg = 2.*__cosf(new_val-latt[rand_ind])-2.;
-  for(int i = 0; i < latt_len; i++)
-    delta_nrg -= 2*__cosf(new_val-latt[i])-2*__cosf(latt[rand_ind]-latt[i]);
-  delta_nrg /= latt_len;
-  if(rands[index] < exp(-delta_nrg/temp) || delta_nrg < 0) {
-    latt[rand_ind] = new_val;
-  }
-}
-	
-void find_xy_parameters(float temp, float *latt, unsigned int latt_len, unsigned int num_steps, float *nrg, float *mag) {
-  cudaError_t cuda_ret;
-	curandGenerator_t gen;
-  dim3 grid_dim = dim3((int)ceil(((float)latt_len)/BLOCK_SIZE), 1, 1);
-  dim3 block_dim = dim3(BLOCK_SIZE, 1, 1);
-  
-  int arr_len = latt_len;
+  if(index >= latt_len) return;
+  //__shared__ float temp_latt[BLOCK_SIZE];
+  curandState s = states[index];
+  curand_init(1234, index, 0, &s);
 
-  unsigned int *rand_inds_d;
-  float *rand_elems_d, *rands_d;
-  cuda_ret = cudaMalloc((void **)&rand_inds_d, arr_len * sizeof(int));
-  if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
-  cuda_ret = cudaMalloc((void **)&rand_elems_d, arr_len * sizeof(float));
-  if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
-  cuda_ret = cudaMalloc((void **)&rands_d, arr_len * sizeof(float));
-  if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
-
-	curandCreateGenerator(&gen, CURAND_RNG_QUASI_SOBOL32);
-	curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);
+	latt[index] = curand_uniform(&s) * PI_2;
+	__syncthreads();
 
   for(int i = 0; i < num_steps; i++) {
-		curandGenerateUniform(gen, rand_elems_d, arr_len);
-		curandGenerateUniform(gen, rands_d, arr_len);
-		curandGenerate(gen, rand_inds_d, arr_len);
-
-    iterate_nrg<<<grid_dim, block_dim>>>(temp, latt, latt_len, rand_inds_d, rand_elems_d, rands_d, nrg);
+    unsigned int rand_ind = index;
+    float new_val = curand_uniform(&s) * PI_2;
+    float old_val = latt[rand_ind];
+    float delta_nrg = 2.*__cosf(new_val-latt[rand_ind])-2.;
+		/*
+    for(int off = 0; off < latt_len; off += BLOCK_SIZE) {
+      temp_latt[threadIdx.x] = latt[off+threadIdx.x];
+      __syncthreads();
+      for(int j = 0; j < BLOCK_SIZE; j++)
+        delta_nrg -= 2*__cosf(new_val-temp_latt[j+off])-2*__cosf(old_val-temp_latt[j+off]);
+    }
+    delta_nrg /= latt_len;
+    if(curand_uniform(&s) < exp(-delta_nrg/temp) || delta_nrg < 0) {
+      latt[rand_ind] = new_val;
+    }
+		*/
+		for(int j = 0; j < latt_len; j++)
+			delta_nrg -= 2*__cosf(new_val-latt[j])-2*__cosf(old_val-latt[j]);
+		delta_nrg /= latt_len;
+    if(curand_uniform(&s) < exp(-delta_nrg/temp) || delta_nrg < 0)
+      latt[rand_ind] = new_val;
   }
-	cudaDeviceSynchronize();
+}
+
+void find_xy_parameters(float temp, float *latt, unsigned int latt_len, unsigned int num_steps, float *nrg, float *mag) {
+  cudaError_t cuda_ret;
+  dim3 grid_dim = dim3((int)ceil(((float)latt_len)/BLOCK_SIZE), 1, 1);
+  dim3 block_dim = dim3(BLOCK_SIZE, 1, 1);
+
+  curandState *states;
+  cuda_ret = cudaMalloc((void **)&states, latt_len*sizeof(curandState));
+  if(cuda_ret != cudaSuccess) FATAL("Unable to alocate device memory");
+
+  iterate_nrg<<<grid_dim, block_dim>>>(temp, latt, latt_len, num_steps, states, nrg);
+
+  cudaDeviceSynchronize();
   calc_energy<<<grid_dim, block_dim>>>(latt, latt_len, nrg);
 
-	cudaFree(rand_inds_d);
-	cudaFree(rand_elems_d);
-	cudaFree(rands_d);
+  cudaFree(states);
+
 }
 
 
