deb80cde3fb20676e2020511477e0a4aa962c97c
Matthew Tomei <tomei2@illinois.edu>
2014-12-06 17:48:35 -0600

    added shared array in lattice perturbation code, slowed down

--- kernel.cu
+++ kernel.cu
@@ -67,6 +67,7 @@ __device__ float calc_delta_nrg(float new_val, float old_val, float *latt, int l
 	return delta_nrg;
 }
 __device__ void perturb_latt(float *latt, int latt_len, int num_steps, float temp, int index, curandState s) {
+	/*
 	float old_val = latt[index];
 	for(int i = 0; i < num_steps; i++) {
 		float new_val = curand_uniform(&s) * PI_2;
@@ -75,12 +76,32 @@ __device__ void perturb_latt(float *latt, int latt_len, int num_steps, float tem
 			latt[index] = new_val;
 			old_val = new_val;
 		}
+		__syncthreads();
+	}
+	*/
+	__shared__ float temp_latt[BLOCK_SIZE];
+	float old_val = latt[index];
+	for(int i = 0; i < num_steps; i++) {
+		float new_val = curand_uniform(&s) * PI_2;
+		float delta_nrg = calc_delta_nrg(new_val, old_val, latt, latt_len);
+		for(int off = 0; off < latt_len; off += BLOCK_SIZE) {
+			temp_latt[threadIdx.x] = latt[off+threadIdx.x];
+			__syncthreads();
+			for(int j = 0; j < BLOCK_SIZE; j++)
+				delta_nrg -= 2*__cosf(new_val-temp_latt[j])-2*__cosf(old_val-temp_latt[j]);
+		}
+		delta_nrg /= latt_len;
+		if(curand_uniform(&s) < exp(-delta_nrg/temp) || delta_nrg < 0) {
+			latt[index] = new_val;
+			old_val = new_val;
+		}
+		__syncthreads();
 	}
 }
 __global__ void iterate_nrg(int num_temps, float **latt_arr, unsigned int latt_len, int num_steps, curandState *states, float *Enrg, int temp_i, float temp) {
 	int index = blockIdx.x * BLOCK_SIZE + threadIdx.x;
   if(index >= latt_len) return;
-  //__shared__ float temp_latt[BLOCK_SIZE];
+  
 
 	curandState s;
 	if(temp_i == 0) {
@@ -141,18 +162,6 @@ __global__ void iterate_nrg(int num_temps, float **latt_arr, unsigned int latt_l
 }
 
 
-			/*
-				for(int off = 0; off < latt_len; off += BLOCK_SIZE) {
-				temp_latt[threadIdx.x] = latt[off+threadIdx.x];
-				__syncthreads();
-				for(int j = 0; j < BLOCK_SIZE; j++)
-        delta_nrg -= 2*__cosf(new_val-temp_latt[j+off])-2*__cosf(old_val-temp_latt[j+off]);
-				}
-				delta_nrg /= latt_len;
-				if(curand_uniform(&s) < exp(-delta_nrg/temp) || delta_nrg < 0) {
-				latt[rand_ind] = new_val;
-				}
-			*/
 
 void find_xy_parameters(int num_temps, float **latt_arr, unsigned int latt_len, unsigned int num_steps, float *Enrg, float *Magn) {
   cudaError_t cuda_ret;
